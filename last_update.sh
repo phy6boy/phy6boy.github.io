eb29adc7749e33cae7ca25264a12e63117590af4
phy6boy <muhsinibnalazeez@gmail.com>
2023-10-22 22:10:21 +0530

    Add Blog

---
+++ last_update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+file='index.html'
+echo "Updating file: $file" 
+sed -i "s/.*last updated.*/last updated $(date '+%d %B %Y')/g" $file
+
+
+file='publications/index.html'
+echo "Updating file: $file" 
+sed -i "s/.*last updated.*/last updated $(date '+%d %B %Y')/g" $file
+
+
+file='research/index.html'
+echo "Updating file: $file" 
+sed -i "s/.*last updated.*/last updated $(date '+%d %B %Y')/g" $file
+
+file='blog/index.html'
+echo "Updating file: $file" 
+sed -i "s/.*last updated.*/last updated $(date '+%d %B %Y')/g" $file
