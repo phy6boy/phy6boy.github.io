aebd4c44f8c6025eb981ef42e7c961b501e98ed8
phy6boy <muhsinibnalazeez@gmail.com>
2023-10-25 22:20:01 +0530

    Update style

--- last_update.sh
+++ last_update.sh
@@ -17,3 +17,7 @@ sed -i "s/.*last updated.*/last updated $(date '+%d %B %Y')/g" $file
 file='blog/index.html'
 echo "Updating file: $file" 
 sed -i "s/.*last updated.*/last updated $(date '+%d %B %Y')/g" $file
+
+file='blog/diffeqpy.html'
+echo "Updating file: $file" 
+sed -i "s/.*last updated.*/last updated $(date '+%d %B %Y')/g" $file
